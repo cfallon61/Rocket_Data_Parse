4e9c6b102b7444e6401109015b74efb9b1febff2
cfallon61 <fallon2@purdue.edu>
2018-08-10 21:37:31 -0400

    Added comments

--- data_parse.py
+++ data_parse.py
@@ -15,19 +15,19 @@ event_lines = []
 def read_data(filename):
     line_count = 0
     with open(filename, "r") as data:
-        for line in data:
+        for line in data:       #for each line in the data file, read it
             line = line.strip()     #strips off new line characters
             if line.find("Parachute_Deployed") != -1 or line.find("Launch_Detect") != -1 or line.find("Landed") != -1: #if these lines are found, add them to a list
                 event_lines.append(str(line_count) + ": " + line)
                 continue
-            data_line = line.split('\t')
+            data_line = line.split('\t')        #if valid data is found, split it around the delimiting '\t'
             i = 0
-            for element in data_line:
+            for element in data_line:       #for each data value in the line of data, add it to the raw data list
                 raw_data[i].append(element)
                 i = i + 1
             line_count = line_count + 1
 
-def get_list_of_data_values(size):
+def get_list_of_data_values(size):      #returns a list of data values, from 0 to size
     i = 0;
     count = []
     while i < size:
@@ -35,23 +35,23 @@ def get_list_of_data_values(size):
         i = i + 1
     return count
 
-def plot_raw(graph_title, axe_label, data_count, values = []):
+def plot_raw(graph_title, axe_label, data_count, values = []):      #plots the data to a graph and saves the graph to a file
 
-    extension = str(datetime.now().time())
+    extension = str(datetime.now().time())      #makes a new file name which is based on the graph title and the current date/time
     extension = extension.replace(":", "_")
 
-    if graph_title.find('Acceleration') != -1:
+    if graph_title.find('Acceleration') != -1:  #if the title is Acceration data, then the filename contains Accel_Data
         fileName = "Accel_Data"
-    elif graph_title.find("Gyro") != -1:
+    elif graph_title.find("Gyro") != -1:  #if the title is Gyro , then the filename contains Gyro_Data
         fileName = "Gyro_Data"
-    else:
+    else:       #no other titles, so it must be temp data
         fileName = "Temp_data"
 
-    fileName = fileName + "_" + extension + ".html"
-    list = get_list_of_data_values(data_count)
-    traces = []
+    fileName = fileName + "_" + extension + ".html"     #append .html to the file because plotly gets passive aggressive
+    list = get_list_of_data_values(data_count)          #gets a list of data points
+    traces = []     #list of graphs
 
-    if len(values) <= 3 and len(values) > 0:
+    if len(values) <= 3 and len(values) > 0:        #if the list has a length of 3 or less, then it is a list which contains a list of other data, so make len amount of traces
         for i in values:
             trace = go.Scatter(
                     x = list,
@@ -60,7 +60,7 @@ def plot_raw(graph_title, axe_label, data_count, values = []):
                     mode = 'lines',
             )
             traces.append(trace)
-    elif len(values) > 3:
+    elif len(values) > 3:       #if the length of the list is greater than 3, then it contains only one type of data
         trace = go.Scatter(
             x=list,
             y=values,
@@ -68,19 +68,20 @@ def plot_raw(graph_title, axe_label, data_count, values = []):
             mode='lines',
         )
         traces.append(trace)
-    else:
+    else:       #if the list is empty, there is no data to plot so return
         return
 
-    layout = dict(
+    layout = dict(      #draws a new layout using the graph title as the title
         title = graph_title,
         xaxis = dict(title = 'Data Points'),
         yaxis = dict(title = axe_label)
     )
 
-    fig = dict(data = traces, layout = layout)
-    plot.offline.plot(fig, filename = fileName)
+    fig = dict(data = traces, layout = layout)  #makes a new figure object
+    plot.offline.plot(fig, filename = fileName)     #plots the information in an html page
 
 def plot_accel(axes = []):
+    #if the list provided is empty, then return from the method
     axes = is_valid_selection_list(axes)
     if axes == False:
         return -1
@@ -97,6 +98,7 @@ def plot_accel(axes = []):
     plot_raw("Acceleration", "Force (G's)", len(raw_data[0]), data_list)
 
 def plot_gyro(axes = []):
+    #if the list provided is empty, then return from the method
     axes = is_valid_selection_list(axes)
     if axes == False:
         return -1
@@ -116,12 +118,13 @@ def plot_temp():
     plot_raw("Temperature", "Temp (Celsius)", len(raw_data[0]), raw_data[6])
 
 def is_valid_selection_list(list = []):
+    #if the list is empty then return that it is not valid
     if list is None:
         print("Empty Axis List!")
         return False
 
     axis_list = []
-
+    #otherwise it is valid and return a list of the axes to be plotted
     if list.count("X Axis") == 1:
         axis_list.append(AXES.X_AXIS)
     if list.count("Y Axis") == 1:
--- gui.py
+++ gui.py
@@ -4,30 +4,30 @@ import easygui as gui
 
 def main():
     try:
-        filename = gui.fileopenbox("", "Data File")
+        filename = gui.fileopenbox("", "Data File")         #try to open the data file for parsing
     except TypeError:
         return
-    parse.read_data(filename)
+    parse.read_data(filename)       #read the data from the file
     get_data_choices()
 
 def get_data_choices():
     options = ["Acceleration", "Gyro", "Temp"]
     choices = gui.multchoicebox("Choose the data to be plotted.", "Sensors", options)
-    if choices is None:
+    if choices is None:     #if there is no choice selected, stop running the program
         return -1
 
-    if choices.count("Acceleration") == 1:
+    if choices.count("Acceleration") == 1:      #if Acceleration is checked parse the acceleration data
         selections = get_axis_choices()
         parse.plot_accel(selections)
 
-    if choices.count("Gyro") == 1:
+    if choices.count("Gyro") == 1:      #if gyro is checked parse the gyro data
         selections = get_axis_choices()
         parse.plot_gyro(selections)
 
-    if choices.count("Temp") == 1:
+    if choices.count("Temp") == 1:      #if temp is checked parse the temp data
         parse.plot_temp()
 
-def get_axis_choices():
+def get_axis_choices():     #get the axes which are to be plotted
     choices = ["X Axis", "Y Axis", "Z Axis"]
     selections = gui.multchoicebox("Choose the Axes to be plotted.", "Axes", choices)
     return selections
